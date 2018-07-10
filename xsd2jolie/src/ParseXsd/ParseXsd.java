6eb2bc58ad9cabcdeb0f01824e10d7a289a555ea
Balint Maschio <bmaschio77@gmail.com>
2018-07-10 17:00:46 +0200

    Type inverted order
    Type inverted order

--- xsd2jolie/src/ParseXsd/ParseXsd.java
+++ xsd2jolie/src/ParseXsd/ParseXsd.java
@@ -5,7 +5,10 @@
  */
 package ParseXsd;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Queue;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -40,16 +43,23 @@ public class ParseXsd {
     public ParseXsd(Document doc){
        
         this.doc = doc;
+		
         
       
     }
     public void parse(){
+	Queue<Node> elementQueue = Collections.asLifoQueue( new ArrayDeque<Node>());
        NodeList nListComplex = doc.getElementsByTagName("xs:complexType");
-       for (int counter = 0; counter < nListComplex.getLength(); counter++) {
-                Node nNode = nListComplex.item(counter);
-                
+	   for (int counter = 0; counter < nListComplex.getLength(); counter++) {
+	             Node nNode = nListComplex.item(counter);
+                elementQueue.add( nNode );
+	   }
+       while (elementQueue.size()> 0) {
+                Node nNode = elementQueue.poll();
+
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                         Element eElement = (Element) nNode;
+					
+                    Element eElement = (Element) nNode;
                     System.out.println("type " + eElement.getAttribute("name") + ":void{\n");
                 }
                 NodeList childList =  nNode.getChildNodes();
@@ -78,8 +88,8 @@ public class ParseXsd {
                if (eElement.getAttribute("minOccurs")!=""){
                   minOccurs = eElement.getAttribute("minOccurs");
                }
-               if (eElement.getAttribute("maxOccurs")!= ""){
-                if (eElement.getAttribute("maxOccurs")== "unbounded"){
+               if (!"".equals( eElement.getAttribute("maxOccurs") )){
+                if ("unbounded".equals( eElement.getAttribute("maxOccurs") )){
                      maxOccurs = "*";
                 }else{
                      maxOccurs = eElement.getAttribute("maxOccurs");
